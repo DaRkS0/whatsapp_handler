1fbba18e47eeff54bb448960970ed08549fcaa9c
Hussein Ashraf <hphantomass@gmail.com>
2026-07-03 16:40:57 +0300

    ss

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,14 @@
+import { GetDocs } from "$lib/firebase/database/client";
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ params, cookies }) => {
+ const userssss = await GetDocs("Jadeer");
+    const users = userssss.docs.map((u) => {
+      const { name, confirmedAt } = u.data();
+      console.log({confirmedAt})
+      return { name,phone:u.id, confirmedAt:confirmedAt?  confirmedAt.toDate().toLocaleDateString():"" };
+    });
+    return {
+        users
+    }
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
-<script>
-import Users from "$lib/clients_whatsapp.json"
-
-    $effect(() => {
-        console.log("Users", Users);
-    });
+<script lang="ts">
+  import CsvHandler from "$lib/CSVHandler.svelte";
+  let { data } = $props();
 </script>
+
+<div class="h-full w-full flex fle-x-col items-center justify-center">
+  <CsvHandler data={data.users} filename="Users">
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Get All Users
+    </button>
+  </CsvHandler>
+</div>
