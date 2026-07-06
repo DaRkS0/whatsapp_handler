99eb44af0784aad353907fcfcc7f51ee1bce8466
Hussein Ashraf <hphantomass@gmail.com>
2026-07-07 00:15:03 +0300

    day2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,21 @@
     console.log(day1Scans);
     return day1Scans;
   }
+
+  function getDay2Scans(users: typeof data.users) {
+    const day1Scans = users
+      .filter(
+        (user) =>
+          user.ScanTime.length > 0 && !user.ClientName.toLowerCase().includes("test")&&user.NewPatch==="Yes",
+      )
+      .toSorted((a, b) => a.SlotTime.localeCompare(b.SlotTime))
+      .map((user) => {
+        const { ClientName, SlotTime, ScanTime, phone } = user;
+        return { ClientName, phone, SlotTime, ScanTime };
+      });
+    console.log(day1Scans);
+    return day1Scans;
+  }
 </script>
 
 <div class="h-full w-full flex flex-col items-center justify-center gap-6">
@@ -39,5 +54,16 @@
     >
       Get Day 1 Scans
     </button>
+    
+  </CsvHandler>
+
+
+  <CsvHandler data={getDay2Scans(data.users)} filename="Day2Scans">
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Get Day 2 Scans
+    </button>
+    
   </CsvHandler>
 </div>
